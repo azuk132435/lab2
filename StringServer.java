e6d66143a75add80d9d438b89ba2d793983142da
azuk132435 <146765369+azuk132435@users.noreply.github.com>
2023-10-22 16:18:30 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.net.URI;
 
 class StringHandler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     StringBuilder messages = new StringBuilder();
     int count = 0;
 
@@ -14,9 +12,9 @@ class StringHandler implements URLHandler {
                 count ++;
                 messages.append(String.format("%d. %s\n", count, parameters[1]));
                 return messages.toString();
-      
+            }
         }
-        return "404 Not Found!";    
+        return "404 Not Found!";  
     }
 }
 
@@ -26,9 +24,7 @@ class StringServer {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
-
         Server.start(port, new StringHandler());
     }
 }
